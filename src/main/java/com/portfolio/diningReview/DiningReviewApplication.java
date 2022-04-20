5f7837bf20e6ae1cb3954a19de1bee0ba6435855
sol namgung <nandaras0103@gmail.com>
2022-04-20 22:19:49 +0900

    first commit

---
+++ src/main/java/com/portfolio/diningReview/DiningReviewApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.diningReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningReviewApplication.class, args);
+	}
+
+}
